32d82d0b98313dbba7518117dc276641a1a6ef91
Mohamed khaled <116751793+Mohamed-khaled77@users.noreply.github.com>
2024-09-05 15:50:03 +0300

    Add files via upload

---
+++ hidex.js
@@ -0,0 +1,17 @@
+// let no = 536;
+// let nos = no.toString();
+// console.log(nos);
+
+// let fruits = ["Apple", "Banana", "Cherry"];
+// let fur = fruits.join();
+// console.log(result);
+
+// let fruits = ["apple", "banana", "cherry"];
+// delete fruits[1];
+// console.log(fruits); // ["apple", empty, "cherry"]
+
+let array = [1, 2, 3, 4];
+
+array.forEach(function (element) {
+  console.log(element);
+});
